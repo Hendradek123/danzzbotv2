558234083db0b378ef54eef165d84f52571a2790
DanzGantevk75 <102861930+DanzGantevk75@users.noreply.github.com>
2022-06-30 13:01:03 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -5,15 +5,21 @@ Knp kak ${ye} Lagi Nyari Sc Ya?
 
 _MINTA AJA SAMA OWNER KAK FREE, SC TIDAK DI PERJUAL BELIKAN_
 
-WHATSAPP OWNER :
-https://wa.me/6288270863279
-
 📍 *N o t e :* 
 • Jangan lupa minta izin owner sebelum menggunakan scriptnya kak!
 • Jangan Lupa kasih star, follow & kasih credit
 • Dilarang menjual Script Ini!
 • Jika menemukan bug di script, harap lapor owner
 • Dilarang reupload sc, Hanya boleh fork
+
+OPEN SEWABOT/JASARUN 
+LIST HARGA SEWABOT :
+1 MINGGU : 5K
+1 BULAN : 10K
+PERMANEN : 20K
+
+LIST HARGA JASARUN :
+20K PERMANEN, JIKA BOT MATI TERUS²AN BOLEH GANTI SC FREE YA KK! 
 
 conn.sendBut(m.chat, esce, 'Ingfo Bermanfaat', 'Thanks', '.menu', m) 
 }
